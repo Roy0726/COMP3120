6d3e5f80867063fd6434f65eb97a121b4b82e654
woo0726 <cjstndof0726@gmail.com>
2017-12-27 17:22:18 +0900

    modify lab2_Q2

--- lab2_Q2.c
+++ lab2_Q2.c
@@ -6,15 +6,15 @@
 #define GOAL 1000000
 /*
 
-execution time for one thread = 5391
-execution time for two thread = 7639
-execution time for four thread = 7340774
-execution time for eight thread = 14039269
-
+execution time for one thread = 5494
+execution time for two thread = 7940
+execution time for four thread = 7282004
+execution time for eight thread = 27934736
+execution time for two thread with lock = 93115
 */
-
-int count=0;
 void *count_num(void *m);
+void *count_num_lock(void *m);
+pthread_mutex_t lock=PTHREAD_MUTEX_INITIALIZER;
 int main(void)
 {
     struct timeval start,end;
@@ -78,6 +78,19 @@ int main(void)
     gettimeofday(&end,NULL);
 
     printf("execution time for eight thread = %ld\n",((end.tv_sec*1000000+end.tv_usec)-(start.tv_sec*1000000+start.tv_usec)));
+  
+
+    count=0; 
+     gettimeofday(&start,NULL);
+    pthread_create(&th[0],NULL,count_num_lock,NULL);
+    pthread_create(&th[1],NULL,count_num_lock,NULL);
+
+    pthread_join(th[0],NULL);
+    pthread_join(th[1],NULL);
+
+    gettimeofday(&end,NULL);
+    printf("execution time for two thread with lock = %ld\n",((end.tv_sec*1000000+end.tv_usec)-(start.tv_sec*1000000+start.tv_usec)));
+
 
     return 0;
 }
@@ -92,3 +105,17 @@ void *count_num(void *m)
     }
     return 0;
 }
+
+void *count_num_lock(void *m)
+{
+    for(;;)
+    {
+        pthread_mutex_lock(&lock);
+        count++;
+        pthread_mutex_unlock(&lock);
+        if(count==GOAL)
+            break;
+    }
+    return 0;
+}
+
